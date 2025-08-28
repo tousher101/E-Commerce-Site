9fe3a9d5af15b6d67b8862d1b00171af6f24d084
Mohammad Jahid Hasan <tousherfana10@gmail.com>
2025-08-29 07:53:23 +0800

    Add get all product&category Endpoint

--- backend/route/user.js
+++ backend/route/user.js
@@ -56,7 +56,192 @@ route.get('/userinfo',verification,roleAuthorize('USER'), async(req,res)=>{
             }
         })
     }catch(err){console.error(err); res.status(500).json({msg: 'Server Error'})}
-})
+});
+
+//get all Product
+route.get('/allproduct', async(req,res)=>{
+    try{
+        const totalProduct= await prisma.product.count();
+        const page= Number(req.query.page)||1;
+        const skip= (page-1)*limit
+        const limit=Number(req.query.limit)||15;
+        const getAllProduct= await prisma.product.findMany({
+            select:{
+                name:true,
+                description:true,
+                price:true,
+                stock:true,
+                createdAt:true,
+                updatedAt:true,
+                photos:true,
+                size:true,
+                color:true,
+                category:true
+            },
+            skip:skip,
+            take:limit,
+            orderBy:{createdAt:'desc'}
+
+        });
+        res.status(200).json({getAllProduct, totalProduct, totalPage:Math.ceil(totalProduct/limit)})
+    }catch(err){console.error(err); res.status(500).json({msg: 'Server Error'})}
+});
+
+//getproduct By Category
+//category: Mens Fashion
+route.get('/mensfashion',async(req,res)=>{
+    try{
+        const totalMensProduct= await prisma.product.count({
+            where:{category:'MENSFASHION'}
+        });
+        const page= Number(req.query.page)||1;
+        const skip= (page-1)*limit
+        const limit=Number(req.query.limit)||15;
+        const getMensProduct= await prisma.product.findMany({
+            where:{category:'MENSFASHION'},
+            select:{
+                name:true,
+                description:true,
+                price:true,
+                stock:true,
+                createdAt:true,
+                updatedAt:true,
+                photos:true,
+                size:true,
+                color:true,
+                category:true
+            },
+            skip:skip,
+            take:limit,
+            orderBy:{createdAt:'desc'}
+        });
+        res.status(200).json({getMensProduct, totalMensProduct, totalPage:Math.ceil(totalMensProduct/limit)})
+    }catch(err){console.error(err); res.status(500).json({msg: 'Server Error'})}
+});
+
+//category: Women Fashion
+route.get('/womenfashion',async(req,res)=>{
+    try{
+        const totalMensProduct= await prisma.product.count({
+            where:{category:'WOMENFASHION'}
+        });
+        const page= Number(req.query.page)||1;
+        const skip= (page-1)*limit
+        const limit=Number(req.query.limit)||15;
+        const getMensProduct= await prisma.product.findMany({
+            where:{category:'WOMENFASHION'},
+            select:{
+                name:true,
+                description:true,
+                price:true,
+                stock:true,
+                createdAt:true,
+                updatedAt:true,
+                photos:true,
+                size:true,
+                color:true,
+                category:true
+            },
+            skip:skip,
+            take:limit,
+            orderBy:{createdAt:'desc'}
+        });
+        res.status(200).json({getMensProduct, totalMensProduct, totalPage:Math.ceil(totalMensProduct/limit)})
+    }catch(err){console.error(err); res.status(500).json({msg: 'Server Error'})}
+});
+
+//category: Kids Fashion
+route.get('/kidsfashion',async(req,res)=>{
+    try{
+        const totalMensProduct= await prisma.product.count({
+            where:{category:'KIDSFASHION'}
+        });
+        const page= Number(req.query.page)||1;
+        const skip= (page-1)*limit
+        const limit=Number(req.query.limit)||15;
+        const getMensProduct= await prisma.product.findMany({
+            where:{category:'KIDSFASHION'},
+            select:{
+                name:true,
+                description:true,
+                price:true,
+                stock:true,
+                createdAt:true,
+                updatedAt:true,
+                photos:true,
+                size:true,
+                color:true,
+                category:true
+            },
+            skip:skip,
+            take:limit,
+            orderBy:{createdAt:'desc'}
+        });
+        res.status(200).json({getMensProduct, totalMensProduct, totalPage:Math.ceil(totalMensProduct/limit)})
+    }catch(err){console.error(err); res.status(500).json({msg: 'Server Error'})}
+});
+
+//category: Accessories
+route.get('/accessories',async(req,res)=>{
+    try{
+        const totalMensProduct= await prisma.product.count({
+            where:{category:'ACCESSORIES'}
+        });
+        const page= Number(req.query.page)||1;
+        const skip= (page-1)*limit
+        const limit=Number(req.query.limit)||15;
+        const getMensProduct= await prisma.product.findMany({
+            where:{category:'ACCESSORIES'},
+            select:{
+                name:true,
+                description:true,
+                price:true,
+                stock:true,
+                createdAt:true,
+                updatedAt:true,
+                photos:true,
+                size:true,
+                color:true,
+                category:true
+            },
+            skip:skip,
+            take:limit,
+            orderBy:{createdAt:'desc'}
+        });
+        res.status(200).json({getMensProduct, totalMensProduct, totalPage:Math.ceil(totalMensProduct/limit)})
+    }catch(err){console.error(err); res.status(500).json({msg: 'Server Error'})}
+});
+
+//category: Perfume
+route.get('/perfume',async(req,res)=>{
+    try{
+        const totalMensProduct= await prisma.product.count({
+            where:{category:'PERFUME'}
+        });
+        const page= Number(req.query.page)||1;
+        const skip= (page-1)*limit
+        const limit=Number(req.query.limit)||15;
+        const getMensProduct= await prisma.product.findMany({
+            where:{category:'PERFUME'},
+            select:{
+                name:true,
+                description:true,
+                price:true,
+                stock:true,
+                createdAt:true,
+                updatedAt:true,
+                photos:true,
+                size:true,
+                color:true,
+                category:true
+            },
+            skip:skip,
+            take:limit,
+            orderBy:{createdAt:'desc'}
+        });
+        res.status(200).json({getMensProduct, totalMensProduct, totalPage:Math.ceil(totalMensProduct/limit)})
+    }catch(err){console.error(err); res.status(500).json({msg: 'Server Error'})}
+});
 
 
 module.exports=route
