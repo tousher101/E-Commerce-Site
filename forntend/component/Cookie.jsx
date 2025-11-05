e9bb3be677c9549827eb9e72a2d9e74dcb5f3b98
Mohammad Jahid Hasan <tousherfana10@gmail.com>
2025-11-05 16:43:53 +0800

    V 1.0.0

--- forntend/component/Cookie.jsx
+++ forntend/component/Cookie.jsx
@@ -5,8 +5,8 @@ export default function cookieNotification({closeCookie, acceptCookie}){
         <div data-aos='slide-up' className="grid grid-1 gap-2 justify-items-center fixed bottom-0 right-0 z-999 p-2 w-full border-1 border-gray-400 rounded-xl bg-blue-100">
             <div className="flex gap-1 items-center">
                 <img className="h-[30px] w-[30px]" src='/cookie.png'/>
-            <p className="text-xl">This site uses cookies to help improve your user experience. </p>
-            <Link href='/cookie-policy'><p className="text-xl underline">Cookie policy</p></Link> 
+            <p className="lg:text-xl md:text-xl text-xs">This site uses cookies to help improve your user experience. </p>
+            <Link href='/cookie-policy'><p className="lg:text-xl md:text-xl text-xs underline">Cookie policy</p></Link> 
             </div>
             
             <div className="flex justify-items-center gap-4">
