862023554b9369086cd417e667f86795a2ba211e
Mohammad Jahid Hasan <tousherfana10@gmail.com>
2025-11-06 14:25:53 +0800

    V1.0.0

--- backend/middle-wear/globalLimiter.js
+++ backend/middle-wear/globalLimiter.js
@@ -1,6 +1,18 @@
-const rateLimit = require('express-rate-limit')
+import rateLimit from "express-rate-limit";
+
 const globalLimiter = rateLimit({
-    windowMs: 60*60*1000, max: 1000, msg:'Too many request from this IP, Please try again later'
-})
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false,  // Disable the `X-RateLimit-*` headers
+  handler: (req, res, next, options) => {
+    return res.status(options.statusCode).json({
+      success: false,
+      message: "Too many requests, please try again later.",
+    });
+  },
+});
+
+
 
 module.exports=globalLimiter
