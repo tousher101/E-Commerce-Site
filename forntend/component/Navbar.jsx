68a9d917672dc48169fb4dac95526f86672f1d88
Mohammad Jahid Hasan <tousherfana10@gmail.com>
2025-11-07 14:09:53 +0800

    v1.0.0

--- forntend/component/Navbar.jsx
+++ forntend/component/Navbar.jsx
@@ -49,7 +49,11 @@ export default function navBar() {
 
     }
         const goCheckOut=()=>{
-          if(!cartData||cartData.length===0){setMsg('Cart Is Empty'); setType('Error'); return}
+         if (!Array.isArray(cartData) || cartData.length === 0) {
+        setMsg('Cart Is Empty'); 
+        setType('Error'); 
+        return;
+    }
           if(!selectedArea){setMsg('Please Selecte Shipping Area');setType('Error'); return}
         router.push(`/checkout/${selectedArea}`);
       setSelectedArea('');
